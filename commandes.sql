84a298cdf166567273867ee51d78de5cabf9f021
lawass1864 <amwass4@hotmail.com>
2026-02-03 20:46:45 +0100

    creation du repo

---
+++ commandes.sql
@@ -0,0 +1,10 @@
+CREATE TABLE netflix (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titre TEXT,
+    saisons INTEGER,
+    episodes_total INTEGER,
+    acteur_principal TEXT,
+    genre TEXT,
+    annee_sortie INTEGER,
+    note INTEGER
+);
